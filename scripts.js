c1bc108b71ead2f377702a3640bd7aa144ee43b7
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-18 13:07:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 def github() {
-  window.open("https://github.com/AwesomePotatoCodes/")
+  console.log("Opened GitHub profile.");
+  window.open("https://github.com/AwesomePotatoCodes/");
+}
+
+def lolzproj() {
+  console.log("Opened LolzProject.");
+  window.open("https://github.com/LolzProject");
+}
+
+def home() {
+  document.title("AwesomePotatoCodes - Home");
+  console.log("Going to homepage.");
+  window.location.replace = "https://awesomepotatocodes.github.io/";
 }
